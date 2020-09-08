dea8f5ba2c23d6fd1a8f80bedff01e273f548156
Nithivarn-Reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2020-09-07 20:40:07 -0500

    Updates

---
+++ precache-manifest.23b2e73f01c577d671996993f1c4edf5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f517b075337f5ce76e0b9331b253f7e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b92b1b8f2b2cf5a4dcba",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "aecf3c75c2d5a28e5f0a",
+    "url": "/portfolio/static/css/main.f45b9402.chunk.css"
+  },
+  {
+    "revision": "b92b1b8f2b2cf5a4dcba",
+    "url": "/portfolio/static/js/2.119c7b72.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.119c7b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecf3c75c2d5a28e5f0a",
+    "url": "/portfolio/static/js/main.67e59aed.chunk.js"
+  },
+  {
+    "revision": "f89f2099577e680dc7b6",
+    "url": "/portfolio/static/js/runtime-main.70964fd7.js"
+  },
+  {
+    "revision": "6fc482ee46e9d6c38053573d6ecb6625",
+    "url": "/portfolio/static/media/cbit_logo.6fc482ee.png"
+  },
+  {
+    "revision": "51a5fad71f90358e4e65472b007d21fe",
+    "url": "/portfolio/static/media/cuisine-prediction.51a5fad7.png"
+  },
+  {
+    "revision": "e687dffd6fd56267f48d3e32572ed3e2",
+    "url": "/portfolio/static/media/event-planner.e687dffd.jpg"
+  },
+  {
+    "revision": "b369fc3a7ccabea29aa8ef5ca621c88a",
+    "url": "/portfolio/static/media/jumbotran.b369fc3a.jpg"
+  },
+  {
+    "revision": "c5fd4bf7ec51c537cb22d6cc212a2be5",
+    "url": "/portfolio/static/media/leetcode.c5fd4bf7.png"
+  },
+  {
+    "revision": "3c9cf6fcdc06f13027574487c1574117",
+    "url": "/portfolio/static/media/redactor.3c9cf6fc.png"
+  },
+  {
+    "revision": "815f40789226a46453943b3fe7ad1eaf",
+    "url": "/portfolio/static/media/rest.815f4078.png"
+  },
+  {
+    "revision": "67a3aab230374992a133bf0b9dbbe2fd",
+    "url": "/portfolio/static/media/summarizer.67a3aab2.jpg"
+  }
+]);
